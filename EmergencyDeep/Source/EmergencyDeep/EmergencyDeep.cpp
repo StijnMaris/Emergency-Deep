18878e1ad64fd955435de95559c500b26c8decf3
StijnMaris <stijn.maris@me.com>
2019-03-28 23:30:50 +0100

    Revert "create"
    This reverts commit 48060196022bbdf50902bd3476aa2d75796e9cad.

--- EmergencyDeep/Source/EmergencyDeep/EmergencyDeep.cpp
+++
@@ -1,9 +0,0 @@
-#include "EmergencyDeep.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, EmergencyDeep, "EmergencyDeep" );
-
-DEFINE_LOG_CATEGORY(LogED_UI);
-DEFINE_LOG_CATEGORY(LogED_Online);
-DEFINE_LOG_CATEGORY(LogED_Voice);
-DEFINE_LOG_CATEGORY(LogED_Game);
--- EmergencyDeep/Source/EmergencyDeep/Player/EDGrabInterface.cpp
+++
@@ -1,4 +0,0 @@
-#include "EDGrabInterface.h"
-#include "UObject/Interface.h"
-
-// Add default functionality here for any IEDGrabInterface functions that are not pure virtual.
